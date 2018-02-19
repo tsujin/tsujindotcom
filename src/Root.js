c19f5e1f54cadebac02c1e6f9ef09f56d2a0b025
Justin Horn <tsujin@linuxtop>
2018-02-19 16:13:07 -0500

    Add boilerplate for website

---
+++ src/Root.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Assets/css/defaults.scss';
+
+class Root extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Root;
