8321cb91c615cf35ff26a4ffb20766a5cbb8d2fd
Justin Horn <contact@tsujin.com>
2018-02-20 16:11:25 -0500

    added basic header

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="logo">
+          logo
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header;
