4b330d0d712087fb568c8452b625ab441d20c20a
Justin Horn <contact@tsujin.com>
2018-02-20 16:52:42 -0500

    added transition to header

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      opacity: 1,
+    }
+
+    this.handleScroll = () => {
+      if (window.scrollY >= 50) {
+        this.setState({ opacity: .25 });
+      }
+      else {
+        this.setState({ opacity: 1 });
+      }
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll);
+  }
+
   render() {
     return (
-      <header>
+      <header style={{ opacity: this.state.opacity }}>
         <div className="logo">
           logo
         </div>
