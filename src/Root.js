2b463329a58991da1b58e6f1f80faf33bb7e2483
Justin Horn <contact@tsujin.com>
2018-02-20 13:37:14 -0500

    add some boilerplate css

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './Assets/css/defaults.scss';
+import './Assets/css/main.scss';
 
 class Root extends Component {
   render() {
     return (
       <div className="App">
-        
+        Testing...
       </div>
     );
   }
