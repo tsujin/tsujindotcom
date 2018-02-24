10961899e1ac14ca2cefe705359c61d56584c35d
Justin Horn <contact@tsujin.com>
2018-02-24 16:10:30 -0500

    added basic styling for navbar

--- src/components/headerComponent/navbar.js
+++ src/components/headerComponent/navbar.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
+import HomeIcon from '../../Assets/icons/home-icon.svg';
 
 class Navbar extends Component {
   render() {
     return (
-      <nav></nav>
+      <nav>
+        <ul>
+          <li className="home">
+            <a href="#">
+              <HomeIcon className="home-icon" width={45} height={45} />
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="blog">
+            <a href="#">
+              <HomeIcon className="blog-icon" width={45} height={45} />
+              <span>News</span>
+            </a>
+          </li>
+          <li className="portfolio">
+            <a href="#">
+              <HomeIcon className="portfolio-icon" width={45} height={45} />
+              <span>Portfolio</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
