0f627b120902be8020396827f1c9794ce805eb47
Justin Horn <contact@tsujin.com>
2018-02-26 20:15:43 -0500

    shortened navbar, added unique icons for each link

--- src/components/headerComponent/navbar.js
+++ src/components/headerComponent/navbar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import HomeIcon from '../../Assets/icons/home-icon.svg';
-
+import PortfolioIcon from '../../Assets/icons/portfolio-icon.svg';
+import NewsIcon from '../../Assets/icons/news-icon.svg';
 class Navbar extends Component {
   render() {
     return (
@@ -16,13 +17,13 @@ class Navbar extends Component {
             </li>
             <li>
               <NavLink exact to='/blog' activeClassName='active'>
-                <HomeIcon className="blog-icon" width={45} height={45} />
+                <NewsIcon className="blog-icon" width={45} height={45} />
                 <span>News</span>
               </NavLink>
             </li>
             <li>
               <NavLink exact to='/portfolio' activeClassName='active'>
-                <HomeIcon className="portfolio-icon" width={45} height={45} />
+                <PortfolioIcon className="portfolio-icon" width={45} height={45} />
                 <span>Portfolio</span>
               </NavLink>
             </li>
