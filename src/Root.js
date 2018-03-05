f70e20a7be1fcbb3ec7b404c46eeea2d0d3fcc66
Justin Horn <contact@tsujin.com>
2018-03-05 00:24:42 -0500

    restyle page to allow overlay to prevent scrolling

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Nav from './components/navComponent/nav'
 
+import './assets/css/normalize.css'
+
 class Root extends Component {
   constructor() {
     super();
@@ -23,7 +25,7 @@ class Root extends Component {
     // when the nav is open, we will add a listener for clicks anywhere on the page
     if (!this.state.navIsOpen) {
       document.addEventListener('click', this.closeNav, false);
-      this.setState({navTransform: "0"});
+      this.setState({navTransform: '0'});
     } else {
         document.removeEventListener('click', this.closeNav, false);
         this.setState({navTransform: "-100"});
@@ -44,7 +46,7 @@ class Root extends Component {
       this.setState({
         overlayStyle: {
           transition: 'opacity 0.3s',
-          transform: 'translate3d(0%, 0, 0)',
+          transform: '',
           opacity: '1',
         }
       });
@@ -66,8 +68,8 @@ class Root extends Component {
     return (
       <div id="outer-container">
         <div className="overlay" style={this.state.overlayStyle}></div>
+        <Nav onClick={this.toggleNav} transform={this.state.navTransform}/>
         <main id="page-wrap">
-          <Nav onClick={this.toggleNav} transform={this.state.navTransform}/>
           <h1>About me</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
