7c8fec4db966ada1ff0c914e67489ac642e79c43
Justin Horn <contact@tsujin.com>
2018-03-04 23:22:28 -0500

    moved state up from navbar to root, added overlay to de-focus page when navbar is open

--- src/components/navComponent/nav.js
+++ src/components/navComponent/nav.js
@@ -3,71 +3,41 @@ import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import './nav.scss';
 
 class Nav extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isOpen: false,
-      transform: "-100",
-    }
-    this.handleClick = this.handleClick.bind(this);
-    this.handleOuterClick = this.handleOuterClick.bind(this);
-  }
-
-  handleClick() {
-    // when the nav is open, we will add a listener for clicks anywhere on the page
-    if (!this.state.isOpen) {
-      document.addEventListener('click', this.handleOuterClick, false);
-      this.setState({transform: "0"});
-    } else {
-        document.removeEventListener('click', this.handleOuterClick, false);
-        this.setState({transform: "-100"});
-      }
-      // toggle state of the navbar
-      this.setState(prevState => ({
-        isOpen: !prevState.isOpen,
-      }));
-  }
-
-  // handles clicks outside of our component
-  handleOuterClick(e) {
-    this.handleClick();
-  }
-
   render() {
     return (
       <Router>
         <div>
-          <div className="sidenav" style={ { transform: `translate(${ this.state.transform }%, 0%)` } }>
+          <div className="sidenav" style={ { transform: `translate(${ this.props.transform }%, 0%)` } }>
             <button className="close-btn">&times;</button>
             <div className="nav-menu-wrap">
               <div className="nav-menu">
                 <NavLink className="navlink" exact to='/'>
-                  <i class="far fa-address-card"></i>
+                  <i className="far fa-address-card"></i>
                   <span>About</span>
                 </NavLink>
                 <NavLink className="navlink" exact to='/News'>
-                  <i class="far fa-newspaper"></i>
+                  <i className="far fa-newspaper"></i>
                   <span>News</span>
                 </NavLink>
                 <NavLink className="navlink" exact to='/Portfolio'>
-                  <i class="far fa-folder-open"></i>
+                  <i className="far fa-folder-open"></i>
                   <span>Portfolio</span>
                 </NavLink>
               </div>
             </div>
             <div className="social-bar">
               <a href="https://www.linkedin.com/in/tsujin">
-                <i class="fab fa-linkedin"></i>
+                <i className="fab fa-linkedin"></i>
               </a>
               <a href="https://www.facebook.com/justin.horn.904/">
-                <i class="fab fa-facebook-square"></i>
+                <i className="fab fa-facebook-square"></i>
               </a>
               <a href="https://github.com/tsujin">
-                <i class="fab fa-github"></i>
+                <i className="fab fa-github"></i>
               </a>
             </div>
           </div>
-          <span className="hamburger-btn" onClick={this.handleClick}><i class="fas fa-bars"></i></span>
+          <span className="hamburger-btn" onClick={this.props.onClick}><i className="fas fa-bars"></i></span>
         </div>
       </Router>
     )
